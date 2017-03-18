4f676820052c225376b2d13d12009f6f6ac719d1
robertsben <roberts.ben@me.com>
2017-03-18 19:21:07 +0000

    Fix server path, add bundle to docker ignore

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html')
+  res.sendFile(__dirname + '../public/index.html')
 })
 
 app.listen(PORT, (err) => {
