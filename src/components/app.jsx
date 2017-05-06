03f465235c5acf1506be6b3df59da2df531a3719
robertsben <roberts.ben@me.com>
2017-05-06 13:51:23 +0100

    Refactor app to avoid using react component class

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import {List, ListItem} from 'material-ui/List'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <List>
-        <ListItem
-          primaryText="Hi, friend."
-          secondaryText={
-            <p>
-              Look, we're working on some stuff here, okay...
-            </p>
-          }
-        />
-      </List>
-    )
-  }
-}
+const App = () => (
+  <List>
+    <ListItem
+      primaryText="Hi, friend."
+      secondaryText={
+        <p>
+          Look, we're working on some stuff here, okay...
+        </p>
+      }
+    />
+  </List>
+)
+
+export default App
