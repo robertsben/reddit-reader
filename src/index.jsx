bc0951381e8ab40924aaee08403465fffbea34a0
robertsben <roberts.ben@me.com>
2017-05-06 13:54:15 +0100

    Fix the touch tap console error

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import MuiApp from './components/muiappwrapper.jsx'
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
+// @see: http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin()
 ReactDOM.render(<MuiApp />, document.getElementById('app'))
