8d49b8f9ac917476927896bd2b1924a688e37aa6
robertsben <roberts.ben@me.com>
2017-05-06 22:12:39 +0100

    Do the loading and add actions into the reducer

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import reducer from './reducers/index.jsx'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import reducer from './reducers/index.jsx'
 import AppContainer from './containers/app.jsx'
+import loadSubreddit from './actions/loadReddit'
 
-let store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
+
+store.dispatch(loadSubreddit('all'))
 
 // @see: http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin()
@@ -17,4 +21,5 @@ ReactDOM.render(
       <AppContainer />
     </MuiThemeProvider>
   </Provider>
-  , document.getElementById('app'))
+  , document.getElementById('app')
+)
--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import * as types from '../actions/actionTypes'
 
 const initialState = {
-  messages: {
-    primary: "Hello, world!",
-    secondary: <p>We've got some stuff going on here!</p>
+  data: {
+    children: []
   }
 }
 
 export default function reducer(state=initialState, action) {
   switch(action.type) {
+    case types.LOAD_SUBREDDIT_SUCCESS:
+      return {...action.subredditData}
     default:
       return state
   }
