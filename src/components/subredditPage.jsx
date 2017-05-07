0ca65e816a00fa752a1319f510d8942972de5907
robertsben <roberts.ben@me.com>
2017-05-07 19:08:25 +0100

    Add a title to the page to show the sub reddit

--- src/components/subredditPage.jsx
+++ src/components/subredditPage.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
-import { List } from 'material-ui/List'
+import List from 'material-ui/List'
+import Subheader from 'material-ui/Subheader'
 import PostsContainer from '../containers/posts.jsx'
 import loadSubreddit from '../actions/loadReddit'
 
+const styles = {
+  title: {
+    fontSize: '200%'
+  }
+}
+
 export default class SubredditPage extends React.Component {
   componentWillMount() {
-    const { dispatch, match } = this.props
-    dispatch(loadSubreddit(match.params.subreddit || 'all'))
+    const { dispatch } = this.props
+    dispatch(loadSubreddit(this._getSubName()))
   }
 
   render() {
     return (
       <List>
+        <Subheader inset style={styles.title}>{`/r/${this._getSubName()}`}</Subheader>
         <PostsContainer />
       </List>
     )
+  }
+
+  _getSubName() {
+    return this.props.match.params.subreddit || 'all'
   }
 }
